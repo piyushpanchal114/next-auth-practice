462c392cafc8eb13928009682c4a156cdcd18515
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-09-01 23:03:15 +0530

    Login done

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,52 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    // Checking user is already exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
+    }
+
+    // Comparing the Password
+    const validPassword = await bcryptjs.compare(password, user.password);
+
+    if (!validPassword) {
+      return NextResponse.json({ error: "invalid passwrd" }, { status: 400 });
+    }
+
+    // Creating Token Data
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    // Creating token
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login sucessful",
+      success: true,
+    });
+
+    response.cookies.set("token", token, { httpOnly: true });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,37 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
 
-  const onLogin = async () => {};
+  const onLogin = async () => {
+    try {
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login Success", response.data);
+      toast.success("Login Successfully");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login Failed", error.message);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-enter min-h-screen py-2">
@@ -38,7 +59,7 @@ export default function LoginPage() {
         onClick={onLogin}
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
       >
-        Login
+        {buttonDisabled ? "No Login" : "Login"}
       </button>
       <Link href="/signup">Signup here</Link>
     </div>
