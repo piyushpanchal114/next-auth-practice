08dd1b85f2d44e897422c8e3534f4d52bf87b540
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-09-04 19:25:11 +0530

    Logout implemented successfully

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = NextResponse.json({
+      message: "Logout Successful",
+      success: true,
+    });
+    response.cookies.set("token", "", { httpOnly: true });
+    return response;
+  } catch (error: any) {
+    NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
