e818dceffc92fa5577546829fa9c43f3572facfe
piyushpanchal114 <piyushpanchal114@gmail.com>
2023-09-06 08:55:36 +0530

    mail on signup

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 connect();
 
@@ -25,6 +26,8 @@ export async function POST(request: NextRequest) {
     const resp = await newUser.save();
 
     console.log(resp);
+
+    await sendEmail({ email, emailType: "VERIFY", userId: resp._id });
 
     return NextResponse.json(
       { message: "User created successfully", success: true, resp },
